6be847e5a10fe05b1602c806bdfb3b4c88fbb633
AbhiAtiUttam <Abhi@glocalize.net>
2024-07-08 00:32:40 +0530

    initialized the app and implemented authentication

---
+++ src/pages/login.jsx
@@ -0,0 +1,39 @@
+import { CHECK_USER_ROUTE } from "@/utils/ApiRoutes";
+import { firebaseAuth } from "@/utils/FirebaseConfig";
+import axios from "axios";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import Image from "next/image";
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+
+function login() {
+  const handleLogin=async ()=>{
+    const provider=new GoogleAuthProvider()
+    const {user:{displayName:name,email,photoURL:profileImage}}=await signInWithPopup(firebaseAuth,provider)
+    console.log(email)
+    try{
+      if(email){
+        const {data}=await axios.post(CHECK_USER_ROUTE,{email});
+        console.log({data})
+      }
+    }catch(error){
+      console.log("here")
+      console.log(error,"error")
+    }
+  }
+  return <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-6">
+    <div className="flex items-center justify-center gap-2">
+      <Image
+      src="/whatsapp.gif" alt="whatsapp" height={300} width={300}/>
+      <span className="text-7xl text-white">
+        Whatsapp
+      </span>
+    </div>
+    <button className="flex items-center justify-center gap-7 bg-search-input-container-background p-5 rounded-lg " onClick={handleLogin}>
+      <FcGoogle className="text-4xl"/>
+      <span className="text-white text-xl">Login with google</span>
+    </button>
+  </div>;
+}
+
+export default login;
